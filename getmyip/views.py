ced22b628d39bdf07689eecddf3236c1489edf45
fahim ashhab <ashhab@divine-it.net>
2023-02-24 15:12:34 +0600

    give me ip

--- getmyip/views.py
+++ getmyip/views.py
@@ -1,5 +1,33 @@
 from django.http import HttpResponse
+import os
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # set the header
+    from_where = ''
+    access_allow_origin = os.getenv('ACCESS_ALLOW_ORIGIN', '*')
+    response = HttpResponse('', content_type='text/plain')
+    response['Access-Control-Allow-Origin'] = access_allow_origin
+
+    # get IP address
+    headers = [
+        'HTTP_X_FORWARDED_FOR', 'X_FORWARDED_FOR',
+        'HTTP_CLIENT_IP',
+        'HTTP_X_REAL_IP',
+        'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'HTTP_VIA',
+        'REMOTE_ADDR',
+    ]
+    for i in headers:
+        x_forwarded_for = request.META.get(i)
+        ip = x_forwarded_for
+
+        if not ip:
+            response.status_code = 404
+            response.content = b'Not found.'
+        else:
+            response.content = ip.encode('utf-8')
+            return response
